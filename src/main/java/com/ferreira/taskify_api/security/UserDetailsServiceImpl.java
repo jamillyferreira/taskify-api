7bb915031b2395ee10992d22d18ffb9d8cbe89de
jamillyferreira <jamillyferreira039@gmail.com>
2026-06-18 20:45:06 -0400

    feat: implementa UserDetailsServiceImpl na entidade User

---
+++ src/main/java/com/ferreira/taskify_api/security/UserDetailsServiceImpl.java
@@ -0,0 +1,22 @@
+package com.ferreira.taskify_api.security;
+
+import com.ferreira.taskify_api.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado com e-mail: " + username));
+    }
+}

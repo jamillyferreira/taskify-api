57bc61b31434f31839ee61ecd69fa2c9ac1089b5
jamillyferreira <jamillyferreira039@gmail.com>
2026-06-18 20:48:53 -0400

    feat: adiciona controller de task

---
+++ src/main/java/com/ferreira/taskify_api/controller/TaskController.java
@@ -0,0 +1,65 @@
+package com.ferreira.taskify_api.controller;
+
+import com.ferreira.taskify_api.dto.request.TaskRequestDTO;
+import com.ferreira.taskify_api.dto.request.TaskUpdateRequestDTO;
+import com.ferreira.taskify_api.dto.response.TaskResponseDTO;
+import com.ferreira.taskify_api.model.User;
+import com.ferreira.taskify_api.service.TaskService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/tasks")
+public class TaskController {
+    private final TaskService taskService;
+
+    @PostMapping
+    public ResponseEntity<TaskResponseDTO> createTask(
+            @RequestBody @Valid TaskRequestDTO request,
+            @AuthenticationPrincipal User user) {
+        TaskResponseDTO response = taskService.createTask(request, user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TaskResponseDTO> findById(
+            @PathVariable Long id,
+            @AuthenticationPrincipal User user) {
+        TaskResponseDTO response = taskService.findById(id, user);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TaskResponseDTO>> findAll(@AuthenticationPrincipal User user) {
+        List<TaskResponseDTO> response = taskService.findAll(user);
+        return ResponseEntity.ok(response);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<TaskResponseDTO> updateTask(
+            @PathVariable Long id,
+            @AuthenticationPrincipal User user,
+            @RequestBody TaskUpdateRequestDTO request) {
+        TaskResponseDTO response = taskService.updateTask(id, user, request);
+        return ResponseEntity.ok(response);
+    }
+
+    @PatchMapping("/{id}/toggle-complete")
+    public ResponseEntity<Void> toggleCompleted(@PathVariable Long id, @AuthenticationPrincipal User user) {
+        taskService.toggleComplete(id, user);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable Long id, @AuthenticationPrincipal User user) {
+        taskService.deleteTask(id, user);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+}

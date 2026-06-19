8b9d1bda681a84bf135ec7ee7882f1a01d3c5cd9
jamillyferreira <jamillyferreira039@gmail.com>
2026-06-18 20:44:09 -0400

    feat: implementa repository

---
+++ src/main/java/com/ferreira/taskify_api/repository/TaskRepository.java
@@ -0,0 +1,12 @@
+package com.ferreira.taskify_api.repository;
+
+import com.ferreira.taskify_api.model.Task;
+import com.ferreira.taskify_api.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    List<Task> findAllByUser(User user);
+}

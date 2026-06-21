df261476a82823d94fcc44504d9b8f6c665d539a
jamillyferreira <jamillyferreira039@gmail.com>
2026-06-21 11:30:20 -0400

    refactor(task): padroniza RequestMapping com prefixo /api

--- src/main/java/com/ferreira/taskify_api/controller/TaskController.java
+++ src/main/java/com/ferreira/taskify_api/controller/TaskController.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/tasks")
+@RequestMapping("/api/tasks")
 public class TaskController {
     private final TaskService taskService;
 

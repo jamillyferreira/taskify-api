fedcf72ceacbe115841a8c1e918982acdf3f84f6
jamillyferreira <jamillyferreira039@gmail.com>
2026-06-26 09:28:00 -0400

    fix: adiciona limite mínimo no titulo da tarefa

--- src/main/java/com/ferreira/taskify_api/dto/request/task/TaskRequestDTO.java
+++ src/main/java/com/ferreira/taskify_api/dto/request/task/TaskRequestDTO.java
@@ -4,12 +4,14 @@ import com.ferreira.taskify_api.enums.Priority;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 
 import java.time.LocalDate;
 
 @Schema(name = "TaskRequestDTO", description = "Dados para criação de uma nova tarefa")
 public record TaskRequestDTO(
+        @Size(min = 3, message = "Título deve ter no mínimo 3 caracteres")
         @NotBlank(message = "Título é obrigatório")
         String title,
 
